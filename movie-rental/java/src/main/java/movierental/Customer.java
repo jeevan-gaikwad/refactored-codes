78eb1c7441c4c3b48cc9e748b299abcccb3be686
JeevanGaikwad <jcgaikwad@gmail.com>
2016-07-27 00:02:30 +0530

    added html printing by refactoring existing code

--- movie-rental/java/src/main/java/movierental/Customer.java
+++ movie-rental/java/src/main/java/movierental/Customer.java
@@ -6,13 +6,20 @@ public class Customer {
 
     private String _name;
     private List<Rental> _rentals = new ArrayList<Rental>();
+    private double _totalAmount;
+    private int _frequentRenterPoints;
 
     public Customer(String name) {
         _name = name;
+        _totalAmount = 0;
+        _frequentRenterPoints = 0;
     }
 
     public void addRental(Rental arg) {
+        _totalAmount += arg.calculateRent();
+        _frequentRenterPoints += arg.calculateFrequentRenterPoints();
         _rentals.add(arg);
+
     }
 
     public String getName() {
@@ -20,25 +27,43 @@ public class Customer {
     }
 
     public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        String result = "Rental Record for " + getName() + "\n";
+        return header()+ body()+ footer();
+    }
 
-        for (Rental each: _rentals) {
-            double thisAmount = 0;
-            thisAmount = each.calculateRent();
-            frequentRenterPoints += each.calculateFrequentRenterPoints();
+    private String footer() {
+        return "Amount owed is " + _totalAmount + "\n"
+                +"You earned " + _frequentRenterPoints + " frequent renter points";
+    }
 
-            // show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
-        }
-
-        // add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-
+    private String body() {
+        String result="";
+        for(Rental each:_rentals)
+            result += "\t" + each.getMovie().getTitle() + "\t" + each.calculateRent() + "\n";
         return result;
     }
 
+    private String header() {
+        return "Rental Record for " + _name + "\n";
+    }
+
+
+    public String htmlStatement() {
+        return htmlHeader() + htmlBody() + htmlFooter();
+    }
+
+    private String htmlFooter() {
+        return  "<b>Amount owed is "+ _totalAmount +"\n<br>" +
+                "You earned "+ _frequentRenterPoints +" frequent renter points</b>";
+    }
+
+    private String htmlBody() {
+        String result="";
+        for(Rental each:_rentals)
+            result+="\t" + each.getMovie().getTitle() + "\t" + each.calculateRent() + "\n<br>";
+        return result;
+    }
+
+    private String htmlHeader() {
+        return "<h1>Rental Record for "+_name+"</h1>\n";
+    }
 }
--- movie-rental/java/src/test/java/movierental/CustomerTest.java
+++ movie-rental/java/src/test/java/movierental/CustomerTest.java
@@ -1,5 +1,6 @@
 package movierental;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -8,18 +9,11 @@ import static org.junit.Assert.*;
  * Created by jeevan on 24/7/16.
  */
 public class CustomerTest {
+
+    private Customer cust1;
+
     @Test
     public void statement() throws Exception {
-        Movie mov1=new Movie("Jab we met",Movie.NEW_RELEASE);
-        Movie mov2=new Movie("Om Shanti Om",Movie.REGULAR);
-        Movie mov3 = new Movie("Bal Ganesh", Movie.CHILDRENS);
-
-        Rental rent1=new Rental(mov1,4);
-        Rental rent2=new Rental(mov2,3);
-        Rental rent3=new Rental(mov3,4);
-
-        CustomerBuilder customerBuilder=new CustomerBuilder().withName("Jeevan").withRentals(rent1,rent2,rent3);
-        Customer cust1 = customerBuilder.build();
         String consoleOutput = cust1.statement();
 
         assertEquals("Rental Record for Jeevan\n" +
@@ -29,6 +23,32 @@ public class CustomerTest {
                 "Amount owed is 18.5\n" +
                 "You earned 4 frequent renter points",
                 consoleOutput);
+    }
+    @Before
+    public void buildCustomer() {
+        Movie mov1=new Movie("Jab we met",Movie.NEW_RELEASE);
+        Movie mov2=new Movie("Om Shanti Om",Movie.REGULAR);
+        Movie mov3 = new Movie("Bal Ganesh", Movie.CHILDRENS);
+
+        Rental rent1=new Rental(mov1,4);
+        Rental rent2=new Rental(mov2,3);
+        Rental rent3=new Rental(mov3,4);
+
+        CustomerBuilder customerBuilder=new CustomerBuilder().withName("Jeevan").withRentals(rent1,rent2,rent3);
+        cust1 = customerBuilder.build();
+    }
+
+    @Test
+    public void htmlStatement(){
+
+        String htmlOutput = cust1.htmlStatement();
+
+        assertEquals("<h1>Rental Record for Jeevan</h1>\n" +
+                    "\tJab we met\t12.0\n<br>" +
+                    "\tOm Shanti Om\t3.5\n<br>" +
+                    "\tBal Ganesh\t3.0\n<br>" +
+                    "<b>Amount owed is 18.5\n<br>" +
+                    "You earned 4 frequent renter points</b>", htmlOutput);
     }
 
 }
